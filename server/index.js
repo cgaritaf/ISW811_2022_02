b9ef05f02ec3483264faf120d085a8bbdbb86390
Cristhian Garita Fonseca <chgari@Cristhians-MacBook-Pro.local>
2022-06-06 15:28:50 -0600

    Lección04

--- server/index.js
+++ server/index.js
@@ -1,88 +1,54 @@
-//Inclusión de la librería
+const dotEnv = require("dotenv");
+const express = require("express");
+const cors = require("cors");
+const logger = require("morgan");
 const mongoose = require("mongoose");
-const {Schema, model} = require("mongoose");
+const app = express();
+
+// Routes
+
+const facturasRouter = require("./routes/facturaRoutes");
+
+// esta linea ayuda a leer la configuracion que tenemos en el archivo .env
+dotEnv.config();
+
+// definimos el uri de la base de datos definido en el archivo .env
+const mongoDB = process.env.MONGODB_DATABASE;
+
+// se conecta a la base de datos
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// esta es la conexión a la base de datos la cual usaremos para detectar errores o conexiones
+const db = mongoose.connection;
+
+//  reporta un error en la conexión
+db.on("error", console.error.bind(console, "MongoDB connection error"));
+
+//  cuando se conecta a la BD monstrara este mensaje
+db.once("open", () => console.log("Connected Successfully to DB " + mongoDB));
+
+// se define el puerto que va a escuchar basado en el archivo de configuración .env
+const port = process.env.PORT || 3000;
+
+// usamos el middleware cors para aceptar llamadas cors en nuestro servidor
+app.use(cors());
+// este middleware nos sirve para loggear las llamadas al servidor
+app.use(logger("dev"));
+
+// middleware para manejar requests y respuestas json
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+// todas las rutas las definimos aqui
+app.use("/facturas/", facturasRouter);
 
 
-//Conexión a la base de datos
-mongoose.connect("mongodb://localhost/universidad");
-
-//Mensaje de error con la base de datos en caso de que exista
-mongoose.connection.on("error", function (e) {
-    console.error(e);
+// iniciamos nuestro servidor
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+  console.log("Press CTRL-C to stop\n");
 });
-
-
-//Mensaje de conexión exitosa a la base de datos
-mongoose.connection.once("open", function (e) {
-    console.log('Conexión a la base de datos exitosa')
-});
-
-//Declaración de los schemas
-var Carrera_Schema = Schema(
-    {
-        nombre: String,
-        descripcion: String,
-        activo: {
-            type: Boolean,
-            default: false,
-            require: true
-        }
-    },
-    {timestamps: true}
-);
-
-
-//Declaración de los schemas
-var Estado_Schema = Schema(
-    {
-        nombre: String,
-        descripcion: String
-    },
-    {timestamps: true}
-);
-
-
-var Factura_Schema = Schema(
-    {
-        num_Factura: Number,
-        nom_Cliente: String,
-        dir_Cliente: String,
-        tel_Cliente: String,
-        estado:{
-            type: Schema.Types.ObjectId,
-            require: true,
-            ref: "estados"
-        }
-    },
-    {timestamps: true}
-);
-
-// Creación del modelo
-var model_carrera = model("carreras", Carrera_Schema);
-var model_estado = model("estados", Estado_Schema);
-var model_factura = model("facturas", Factura_Schema);
-
-// Se inserta un objeto utilizando el modelo
-model_carrera.create({ nombre: "Otra", descripcion: "Prueba", activo: true }, 
-    function(err) {
-        if (err) return console.error(err);
-    }
-);
-
-// Se inserta un objeto utilizando el modelo
-model_estado.create({ nombre: "Activa", descripcion: "Prueba" }, 
-    function(err) {
-        if (err) return console.error(err);
-    }
-);
-
-model_factura.create({  num_Factura: 1, 
-                        nom_Cliente: "CGF", 
-                        dir_Cliente: "SJO",
-                        tel_Cliente: "8888",
-                        estado: "62957d1a29108f73c0abd188"
-                    }, 
-    function(err) {
-        if (err) return console.error(err);
-    }
-);
